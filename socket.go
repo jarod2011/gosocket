4fa4b93f0c467f9b23b87ca7886e7e89d96fe7d6
Jarod <xinghen249@gmail.com>
2020-04-28 11:18:05 +0800

    update

---
+++ socket.go
@@ -0,0 +1,10 @@
+package gosocket
+
+import (
+	_ "github.com/jarod2011/gosocket/buffers"
+	_ "github.com/jarod2011/gosocket/client"
+	_ "github.com/jarod2011/gosocket/conn"
+	_ "github.com/jarod2011/gosocket/conn_repo"
+	_ "github.com/jarod2011/gosocket/server"
+	_ "github.com/jarod2011/gosocket/util"
+)
