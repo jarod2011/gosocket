a87eff81721ae3dbca0bf4a955b0d7cfbd2a4e3c
Jarod <xinghen249@gmail.com>
2020-04-28 11:24:53 +0800

    Update socket.go

--- socket.go
+++ socket.go
@@ -2,9 +2,17 @@ package gosocket
 
 import (
 	_ "github.com/jarod2011/gosocket/buffers"
-	_ "github.com/jarod2011/gosocket/client"
+	"github.com/jarod2011/gosocket/client"
 	_ "github.com/jarod2011/gosocket/conn"
 	_ "github.com/jarod2011/gosocket/conn_repo"
-	_ "github.com/jarod2011/gosocket/server"
+	"github.com/jarod2011/gosocket/server"
 	_ "github.com/jarod2011/gosocket/util"
 )
+
+func NewServer(handler server.Handler, opts ...server.Option) server.Server {
+	return server.New(handler, opts...)
+}
+
+func NewClient(opts ...client.Option) client.Client {
+	return client.New(opts...)
+}
