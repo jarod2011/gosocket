b7d87467f4426740c981d7eca1f7fb381d2e03d3
Jarod <xinghen249@gmail.com>
2020-05-02 10:44:03 +0800

    add some method

--- socket.go
+++ socket.go
@@ -1,12 +1,15 @@
 package gosocket
 
 import (
+	"github.com/jarod2011/gosocket/buffers"
 	_ "github.com/jarod2011/gosocket/buffers"
 	"github.com/jarod2011/gosocket/client"
+	"github.com/jarod2011/gosocket/conn"
 	_ "github.com/jarod2011/gosocket/conn"
 	_ "github.com/jarod2011/gosocket/conn_repo"
 	"github.com/jarod2011/gosocket/server"
 	_ "github.com/jarod2011/gosocket/util"
+	"net"
 )
 
 func NewServer(handler server.Handler, opts ...server.Option) server.Server {
@@ -15,4 +18,12 @@ func NewServer(handler server.Handler, opts ...server.Option) server.Server {
 
 func NewClient(opts ...client.Option) client.Client {
 	return client.New(opts...)
+}
+
+func NewConn(cc net.Conn) conn.Conn {
+	return conn.New(cc)
+}
+
+func NewBufferPool() buffers.Buffers {
+	return buffers.New()
 }
--- socket_test.go
+++ socket_test.go
@@ -1,1 +1,26 @@
 package gosocket
+
+import (
+	"github.com/jarod2011/gosocket/conn"
+	"net"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	NewServer(func(cc conn.Conn, notifyClose <-chan struct{}) error {
+		return nil
+	})
+}
+
+func TestNewClient(t *testing.T) {
+	NewClient()
+}
+
+func TestNewConn(t *testing.T) {
+	c1, _ := net.Pipe()
+	NewConn(c1)
+}
+
+func TestNewBufferPool(t *testing.T) {
+	NewBufferPool()
+}
